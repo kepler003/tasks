c903e027e42844028d20369f58f83b2649dc55a8
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-10-08 16:06:54 +0200

    Add storage

---
+++ src/scripts/data.js
@@ -0,0 +1,57 @@
+
+let storage = {
+  employees: [
+    {
+      id: 0,
+      name: 'Adam Nowak',
+      src: './images/man1',
+      tasks: [
+        {
+          name: 'Zadanie 1',
+          pricePLN: 2121,
+          priceEUR: 122
+        },
+        {
+          name: 'Zadanie 2',
+          pricePLN: 2121,
+          priceEUR: 122
+        },
+        {
+          name: 'Zadanie 3',
+          pricePLN: 2121,
+          priceEUR: 122
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: 'Michał Potoczek',
+      src: './images/man2',
+      tasks: [
+        {
+          name: 'Zadanie 1',
+          pricePLN: 2121,
+          priceEUR: 122
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Antoni Worek',
+      src: './images/man3',
+      tasks: [
+        {
+          name: 'Zadanie 1',
+          pricePLN: 2121,
+          priceEUR: 122
+        },
+        {
+          name: 'Zadanie 2',
+          pricePLN: 2121,
+          priceEUR: 122
+        }
+      ]
+    }
+  ],
+  chosenEmployee: undefined
+}
