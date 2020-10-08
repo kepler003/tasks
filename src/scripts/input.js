8a5156292d7c537b00ad0683ffb7f5b93156d23e
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-10-08 12:51:04 +0200

    Add validation for company name

---
+++ src/scripts/input.js
@@ -0,0 +1,81 @@
+
+const $ = require('jquery');
+const { errorTemplate } = require('./templates');
+
+
+// Label
+
+$('.js-input').on('keyup', (e) => {
+  checkIfHasValue(e);
+})
+
+const checkIfHasValue = (e) => {
+  const length = e.target.value.length;
+  
+  if(length !== 0) makeHasValue(e.target);
+  else             undoHasValue(e.target);
+}
+
+const makeHasValue = (input) => {
+  $(input).addClass('input--hasValue');
+}
+
+const undoHasValue = (input) => {
+  $(input).removeClass('input--hasValue');
+}
+
+
+// Validation
+
+$('.js-input[data-validate="text"').on('keyup', (e) => {
+  validateText(e);
+});
+
+const validateText = (e) => {
+  const value = e.target.value;
+  const regex = /^[\s\p{L}.-]+$/u;
+
+  if(value.trim().length === 0) {
+    makeInvalid(e.target, 'To pole nie może być puste');
+    return;
+  } else if(!value.match(regex)) {
+    makeInvalid(e.target, 'To pole może zawierać duże i małe litery, myślniki i kropki');
+    return;
+  }
+  
+  undoInvalid(e.target);
+}
+
+const makeInvalid = (input, message) => {
+  styleInvalid(input);
+  addError(input, message);
+}
+
+const undoInvalid = (input) => {
+  unstyleInvalid(input);
+  removeError(input);
+}
+
+const styleInvalid = (input) => {
+  $(input).addClass('input--invalid');
+}
+
+const unstyleInvalid = (input) => {
+  $(input).removeClass('input--invalid');
+}
+
+const addError = (input, message) => {
+  const error = $(input).siblings('.input__error')[0];
+  const inputBox = $(input).parent('.input__box');
+  
+  if(!error) {
+    $(inputBox).append(errorTemplate(message));
+  } else {
+    $(error).text(message);
+  }
+}
+
+const removeError = (input, message) => {
+  const error = $(input).siblings('.input__error')[0];
+  $(error).remove();
+}
---
+++ src/scripts/templates.js
@@ -0,0 +1,10 @@
+
+const errorTemplate = (error) => {
+  return `
+    <span class="input__error">${error}</span>
+  `;
+}
+
+module.exports = {
+  errorTemplate
+}
