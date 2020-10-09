344c2da14d1ccc4baae9c0ac9ac5ea0f53814669
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-10-09 18:55:25 +0200

    Create validation for add task form

--- src/scripts/addTask.js
+++ src/scripts/addTask.js
@@ -1,15 +1,25 @@
 
 const $ = require('jquery');
-const { addTask } = require('./storage');
+const {addTask}            = require('./storage');
+const {validateText}       = require('./input');
+const {checkIfFormIsValid} = require('./form');
 
 
 $('.js-add-task__form').on('submit', (e) => {
   e.preventDefault();
-  createTask(e);
+  validateForm(e);
+  submitForm(e);
 });
 
 
-const createTask = (e) => {
+const validateForm = (e) => {
+  const dataText = $(e.target).find('.js-input[data-text]');
+  if(dataText.length) validateText($(dataText));
+}
+
+const submitForm = (e) => {
+  if(!checkIfFormIsValid($(e.target))) return;
+
   const dataArr = $(e.target).serializeArray();
   const task = {
     name: dataArr[0].value,
